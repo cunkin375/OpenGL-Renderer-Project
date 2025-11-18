71813667eb67f7ab8b60725beb184ee615f704cd
ali <159713281+cunkin375@users.noreply.github.com>
2025-11-17 22:24:38 -0500

    remove model matrix

--- OpenGL-Core/src/util/Camera.cpp
+++ OpenGL-Core/src/util/Camera.cpp
@@ -1,13 +1,18 @@
 #include "Camera.h"
+#include "core/Window.h"
 #include <gtc/type_ptr.hpp>
 
 void Camera::update()
 {
-    m_Model = glm::mat4(1.0f);
-    m_Model = glm::rotate(m_Model, (GLfloat)glfwGetTime(), glm::vec3{1.0f, 1.0f, 1.0f});
+    m_View  = glm::mat4{1.0f};
+    m_Projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    m_View  = glm::translate(m_View, glm::vec3{0.0f, 0.0f, -3.0f});
 }
 
-void Camera::transform(GLuint transformLoc)
+void Camera::transform(GLuint shaderID)
 {
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(m_Model));
+    GLuint viewMatrixLoc = glGetUniformLocation(shaderID, "view");
+    GLuint projectionMatrixLoc = glGetUniformLocation(shaderID, "projection");
+    glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, glm::value_ptr(m_View));
+    glUniformMatrix4fv(projectionMatrixLoc, 1, GL_FALSE, glm::value_ptr(m_Projection));
 }
--- OpenGL-Core/src/util/Camera.h
+++ OpenGL-Core/src/util/Camera.h
@@ -9,21 +9,20 @@
 // Transformation of coordinates into normalized device coordinates
 // Pipeline:
 // > Model Coordinates -> World Coordinates -> View Coordinates -> Clip Coordinates -> NDC -> Screen Coordinates
+// > clipVertex = projectionMatrix * viewMatrix * localVertex
 
 
 class Camera
 {
 public:
     void update();
-    void transform(GLuint transformLoc);
+    void transform(GLuint programID);
 
     inline const glm::mat4 getProjection() { return m_Projection; }
     inline const glm::mat4 getView()       { return m_View; }
 
  private:
-    glm::mat4 m_Projection {1.0f};
-    glm::mat4 m_Model      {1.0f};
-    glm::mat4 m_View       {1.0f};
-
+    glm::mat4 m_View {1.0f};
+    glm::mat4 m_Projection;
 
 };
