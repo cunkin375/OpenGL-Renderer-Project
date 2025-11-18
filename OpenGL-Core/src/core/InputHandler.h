d07801de56f1103aa36d2831959d9276cde28495
ali <159713281+cunkin375@users.noreply.github.com>
2025-11-17 19:51:49 -0500

    make InputHandler namespace

--- OpenGL-Core/src/core/InputHandler.h
+++ OpenGL-Core/src/core/InputHandler.h
@@ -2,15 +2,45 @@
 
 #include "Window.h"
 
-class InputHandler
+namespace InputHandler
 {
-public:
-	static bool isKeyPressed(Window& window, GLint key);
-	static bool isKeyReleased(Window& window, GLint key);
-	static bool isMBPressed(Window& window, GLint button);
-	static bool isMBReleased(Window& window, GLint button);
-	static void setMousePos(Window& window, GLdouble x, GLdouble y);
-	static void disableMouse(Window& window);
-	static void hideMouse(Window& window);
-	static void showMouse(Window& window);
+    inline bool isKeyPressed(const Window& window, GLint key)
+    {
+        return glfwGetKey(window.getHandle(), key) == GLFW_PRESS;
+    }
+
+    inline bool isKeyReleased(const Window& window, GLint key)
+    {
+        return glfwGetKey(window.getHandle(), key) == GLFW_RELEASE;
+    }
+
+    inline bool isMBPressed(const Window& window, GLint button)
+    {
+        return glfwGetMouseButton(window.getHandle(), button) == GLFW_PRESS;
+    }
+
+    inline bool isMBReleased(const Window& window, GLint button)
+    {
+        return glfwGetMouseButton(window.getHandle(), button) == GLFW_RELEASE;
+    }
+
+    inline void setMousePos(const Window& window, GLdouble xPos, GLdouble yPos)
+    {
+        glfwSetCursorPos(window.getHandle(), xPos, yPos);
+    }
+
+    inline void disableMouse(const Window& window)
+    {
+        glfwSetInputMode(window.getHandle(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    }
+
+    inline void hideMouse(const Window& window)
+    {
+        glfwSetInputMode(window.getHandle(), GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+    }
+
+    inline void showMouse(const Window& window)
+    {
+        glfwSetInputMode(window.getHandle(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    }
 };
