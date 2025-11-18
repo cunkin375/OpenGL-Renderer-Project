df9149eee20cb663462e4bcd79357cfaffe50f53
ali <159713281+cunkin375@users.noreply.github.com>
2025-11-17 20:12:26 -0500

    minor refactor, change animation

--- OpenGL-Core/src/util/Camera.cpp
+++ OpenGL-Core/src/util/Camera.cpp
@@ -3,12 +3,11 @@
 
 void Camera::update()
 {
-    m_View = glm::mat4(1.0f);
-    m_View = glm::translate(m_View, glm::vec3(0.5f, -0.5f, 0.0f));
-    m_View = glm::rotate(m_View, (GLfloat)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+    m_Model = glm::mat4(1.0f);
+    m_Model = glm::rotate(m_Model, (GLfloat)glfwGetTime(), glm::vec3{1.0f, 1.0f, 1.0f});
 }
 
 void Camera::transform(GLuint transformLoc)
 {
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(m_View));
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(m_Model));
 }
--- OpenGL-Core/src/util/Camera.h
+++ OpenGL-Core/src/util/Camera.h
@@ -6,6 +6,11 @@
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
 
+// Transformation of coordinates into normalized device coordinates
+// Pipeline:
+// > Model Coordinates -> World Coordinates -> View Coordinates -> Clip Coordinates -> NDC -> Screen Coordinates
+
+
 class Camera
 {
 public:
@@ -13,11 +18,12 @@ public:
     void transform(GLuint transformLoc);
 
     inline const glm::mat4 getProjection() { return m_Projection; }
-    inline const glm::mat4 getView() { return m_View; }
+    inline const glm::mat4 getView()       { return m_View; }
 
-private:
-    glm::mat4 m_Projection = glm::mat4(1.0f);
-    glm::mat4 m_View = glm::mat4(1.0f);
+ private:
+    glm::mat4 m_Projection {1.0f};
+    glm::mat4 m_Model      {1.0f};
+    glm::mat4 m_View       {1.0f};
 
 
 };
