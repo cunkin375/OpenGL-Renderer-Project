e5c21f2586448fb20c5aecf1dc91877d93e25207
ali <159713281+cunkin375@users.noreply.github.com>
2025-11-17 22:23:59 -0500

    refactor shader to support 3d coordinate system

--- OpenGL-Core/assets/shaders/460.shader_vertex.glsl
+++ OpenGL-Core/assets/shaders/460.shader_vertex.glsl
@@ -2,14 +2,17 @@
 
 layout(location = 0) in vec3 aPos; // position variable has attribute 0
 layout(location = 2) in vec2 aTexCoord; // texture coordinate variable has attribute 2
+                                        //
 
-out vec3 vColor; // output color to fragment shader
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec3 vColor;    // output color to fragment shader
 out vec2 vTexCoord; // output texture coordinate to fragment shader
-
-uniform mat4 uTransform;
 
 void main()
 {
-    gl_Position = uTransform * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
     vTexCoord = vec2(aTexCoord.x, aTexCoord.y);
 } // end main
